7ecd72100c5f9c1d44e89fae62f2f5e5e3238c76
Emily <emilybtoliveira@gmail.com>
2022-05-30 10:10:54 -0300

    adiciona as telas maindashboard e completeprofile a

---
+++ p3/resources/views/main_dashboard.blade.php
@@ -0,0 +1,238 @@
+@extends('basic_component')
+
+@section('page content')
+<div class="row align-items-center py-4">
+    <div class="col-2" style="width: min-content;">
+        <button type="button" class="btn btn-dark material-icons" disabled style="background: #212529 !important; opacity: 1 !important;">
+            home
+        </button>
+    </div>
+    <div class="col-10 col-md-11">
+        <h4 class="m-0 mb-1 row discipline-name">
+            Dashboard de Disciplinas
+        </h4>
+    </div>
+</div>
+
+<!--carrossel 1º Periodo-->
+<div class="periodos-containers container">
+    <h4 class="periodos_title">1º período</h4>
+    <div id="carrossel" class="carousel slide" data-ride="carousel" data-interval="false">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row" style="margin-left: 40px; margin-right:40px;">
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 green">
+                            <h5 class="card-title">Programação 1</h5>                    
+                            <a class="stretched-link"></a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 aqua">
+                            <h5 class="card-title">Lógica para computação</h5>                    
+                            <a class="stretched-link"></a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 blue">
+                            <h5 class="card-title">Computação, Sociedade e Ética</h5>                    
+                            <a class="stretched-link"></a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 darkblue">
+                            <h5 class="card-title">Matemática Discreta</h5>                    
+                            <a class="stretched-link"></a>
+                        </div>
+                    </div>                           
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <div class="row" style="margin-left: 40px; margin-right:40px;">
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 darkblue">
+                            <h5 class="card-title">Cálculo Diferencial e Integral</h5>                    
+                            <a class="stretched-link"></a>
+                        </div>
+                    </div>   
+                </div>
+            </div>
+        </div>
+
+        <a style="width: max-content;" class="carousel-control-next pt-2 pb-3" href="#carrossel" role="button" data-slide="next" id="carousel-control">
+            <span class="icon-md text-dark">navigate_next</span>                    
+        </a>
+
+        <a style="width: max-content;" class="carousel-control-prev pt-2 pb-3" href="#carrossel" role="button" data-slide="next" id="carousel-control">
+            <span class="icon-md text-dark">navigate_before</span>                    
+        </a>
+    </div>
+</div>
+
+<!--2 PERIODO-->
+<div class="periodos-containers container">
+    <h4 class="periodos_title">2º período</h4>
+    <div id="carrossel" class="carousel slide" data-ride="carousel" data-interval="false">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row" style="margin-left: 40px; margin-right:40px;">
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 green">
+                            <h5 class="card-title">Estrutura de Dados</h5>                    
+                            <a class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 green">
+                            <h5 class="card-title">Banco de Dados</h5>                    
+                            <a class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 aqua">
+                            <h5 class="card-title">Organização e Arquitetura de Computadores</h5>                    
+                            <a class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 darkblue">           
+                            <h5 class="card-title">Geometria Analítica</h5>                    
+                            <a class="stretched-link"></a>
+                        </div>
+                    </div>                           
+    
+                </div>            
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--3 PERIODO-->
+<div class="periodos-containers container">
+    <h4 class="periodos_title">3º período</h4>
+    <div id="carrossel" class="carousel slide" data-ride="carousel" data-interval="false">
+        <div class="carousel-inner">
+            <div class="carousel-item active">						
+                <div class="row" style="margin-left: 40px; margin-right:40px;">
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 aqua">
+                            <h5 class="card-title">Redes de Computadores</h5>                    
+                            <a class="stretched-link"></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 darkblue">
+                    
+                                <h5 class="card-title">Teoria dos Grafos</h5>                    
+                                <a class="stretched-link"></a>
+    
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 darkblue">
+                    
+                                <h5 class="card-title">Álgebra Linear</h5>                    
+                                <a class="stretched-link"></a>
+    
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 darkblue">
+                    
+                                <h5 class="card-title">Probabilidade e Estatística</h5>                    
+                                <a class="stretched-link"></a>
+    
+                        </div>
+                    </div>                           
+                </div>            
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--4 PERIODO-->
+<div class="periodos-containers container">
+    <h4 class="periodos_title">4º período</h4>
+    <div id="carrossel" class="carousel slide" data-ride="carousel" data-interval="false">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row" style="margin-left: 40px; margin-right:40px;">
+    
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 darkblue">
+                    
+                                <h5 class="card-title">Projeto e Análise de Algoritmos</h5>                    
+                                <a class="stretched-link"></a>
+    
+                        </div>
+                    </div>
+    
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 aqua">
+                    
+                                <h5 class="card-title">Teoria da computação</h5>                    
+                                <a class="stretched-link"></a>
+    
+                        </div>
+                    </div>
+    
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 green">
+                    
+                                <h5 class="card-title">Programação 3</h5>                    
+                                <a class="stretched-link"></a>
+    
+                        </div>
+                    </div>
+    
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 green">
+                    
+                                <h5 class="card-title">Projeto de Software</h5>                    
+                                <a class="stretched-link"></a>
+    
+                        </div>
+                    </div>                           
+    
+                </div>            
+            </div>
+        </div>
+    </div>
+</div>
+    
+<!--Ênfase Sistemas de Informação-->
+<div class="container">
+    <h4 class="periodos_title">Ênfase Sistemas de Informação</h4>
+    <div id="carrossel" class="carousel slide" data-ride="carousel" data-interval="false">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row" style="margin-left: 40px; margin-right:40px;">
+    
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 blue">
+                    
+                                <h5 class="card-title">Interação Homem-Máquina</h5>                    
+                                <a class="stretched-link"></a>
+    
+                        </div>
+                    </div>
+    
+                    <div class="col-md-4 col-lg-3 pt-2 pb-3">
+                        <div class="card dashboard-card slot-2 green">
+                    
+                                <h5 class="card-title">Ciência de Dados</h5>                    
+                                <a class="stretched-link"></a>
+    
+                        </div>
+                    </div>                         
+    
+                </div>            
+            </div>
+        </div>
+    </div>
+</div>
+@stop
--- p3/vendor/composer/autoload_real.php
+++ p3/vendor/composer/autoload_real.php
@@ -22,39 +22,18 @@ class ComposerAutoloaderInitd68220ff58bd85066717fddbf028ed68
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitd68220ff58bd85066717fddbf028ed68', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitd68220ff58bd85066717fddbf028ed68', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInitd68220ff58bd85066717fddbf028ed68::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInitd68220ff58bd85066717fddbf028ed68::getInitializer($loader));
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitd68220ff58bd85066717fddbf028ed68::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
+        $includeFiles = \Composer\Autoload\ComposerStaticInitd68220ff58bd85066717fddbf028ed68::$files;
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequired68220ff58bd85066717fddbf028ed68($fileIdentifier, $file);
         }
@@ -63,11 +42,16 @@ class ComposerAutoloaderInitd68220ff58bd85066717fddbf028ed68
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequired68220ff58bd85066717fddbf028ed68($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
