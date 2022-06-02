d57dcedb1f24bdc18689a1f41d52e02fe18b273c
José Arthur <jals@ic.ufal.br>
2022-06-01 22:13:57 -0300

    Add files via upload

--- p3/routes/web.php
+++ p3/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Profile;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,16 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('basic_component');
 });
 
-
-Route::get('/login', function () {
-    return view('login');
+Route::get('/home', function () {
+    return view('main_dashboard');
 });
 
-Route::get('/cadastro', function () {
-    return view('cadastro');
-});
+Route::get('/profile', [Profile::class, 'completeProfile']);
 
-
+Route::post('/profileComplete', [Profile::class, 'saveProfile']);
